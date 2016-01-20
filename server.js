f8dc50d779fab1802124524411307a0ff678eed9
dungnt <dungnt8805@gmail.com>
2016-01-21 00:16:45 +0700

    chord-1 build application skeleton

---
+++ server.js
@@ -0,0 +1,33 @@
+require('babel-register')
+
+var path = require('path'),
+  express = require('express'),
+  bodyParse = require('body-parse'),
+  compression = require('compression'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  async = require('async'),
+  colors = require('colors'),
+  mongoose = require('mongoose'),
+  request = require('request'),
+  React = require('react'),
+  ReactDom = require('react-dom/server'),
+  Router = require('react-router'),
+  swig = require('swig'),
+  xml2js = require('xml2js'),
+  _ = require('underscore');
+
+var app = express();
+
+app.set('port', process.env.PORT || 80);
+
+app.use(compression());
+
+app.use(logger('dev'));
+
+app.use(bodyParse.json());
+
+app.use(bodyParse.urlencoded({extended: false}));
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(express.static(path.join(__dirname, 'public')));
